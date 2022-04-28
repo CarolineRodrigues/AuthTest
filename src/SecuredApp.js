4c072509bc1c6fa7c340c645e8eabf42d08cdc12
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-04-28 18:29:59 +0100

    init

---
+++ src/SecuredApp.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import App from './App';
+import { AuthProvider, AuthService, useAuth } from 'react-oauth2-pkce';
+const authService = new AuthService({
+  clientId: 'ticapp',
+  authorizeEndpoint: 'https://dev-apigw-2-pdun.northeurope.cloudapp.azure.com:8443/realms/fa/protocol/openid-connect/auth',
+  redirectUri: 'https://oidcdebugger.com/debug',
+  scopes: ['openid'],
+});
+function SecuredApp() {
+  const { authService } = useAuth();
+
+  const login = async () => authService.authorize();
+  const logout = async () => authService.logout();
+
+  if (authService.isPending()) {
+    return (
+      <div>
+        Loading...
+        <button
+          onClick={() => {
+            logout();
+            login();
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    );
+  }
+
+  if (!authService.isAuthenticated()) {
+    return (
+      <div>
+        <p>Not Logged in yet</p>
+        <button onClick={login}>Login</button>
+      </div>
+    );
+  }
+
+  const token = authService.getAuthTokens();
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+      <App />
+    </div>
+  );
+}
+// SecuredApp as before
+function WrappedSecuredApp() {
+  return (
+    <AuthProvider authService={authService}>
+      <SecuredApp />
+    </AuthProvider>
+  );
+}
+
+export default WrappedSecuredApp;
